73ec6fe5816e4a3e2571eff97843a47bc45aeec2
Jake Edgar <jakeedgar1012@gmail.com>
2022-05-04 16:00:45 -0700

    finish UI logic for console app
    Co-authored-by: Caleb Coughenour <CalebCoughenour@protonmail.com>

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,16 +1,45 @@
-// using System;
-// using System.Collections.Generic;
-// using RockPaperScissors;
+using System;
+using System.Collections.Generic;
+using RockPaperScissors;
 
-// public class Program 
-// {
-//   public static void Main() 
-//   {
-//     Console.WriteLine("player one choose rock paper or scissors.");
-//     string playerOne = Console.ReadLine();
-//     Console.WriteLine("player two choose rock paper or scissors.");
-//     string playerTwo = Console.ReadLine();
-//     Game newGame = new Game(playerOne, playerTwo);
-//     newGame.RunGame();
-//   }
-// }
+public class Program
+{
+  public static void Main()
+  {
+    while (true)
+    {
+      Console.WriteLine("player one choose rock paper or scissors.");
+      string playerOne = Console.ReadLine();
+      while (true)
+      {
+        if ((playerOne != "rock") && (playerOne != "paper") && (playerOne != "scissors"))
+        {
+          Console.WriteLine("please enter rock paper or scissors.");
+          playerOne = Console.ReadLine();
+        }
+        else
+        {
+          break;
+        }
+      }
+      Console.WriteLine("player two choose rock paper or scissors.");
+      string playerTwo = Console.ReadLine();
+      while (true)
+      {
+        if ((playerTwo != "rock") && (playerTwo != "paper") && (playerTwo != "scissors"))
+        {
+          Console.WriteLine("please enter rock paper or scissors.");
+          playerTwo = Console.ReadLine();
+        }
+        else
+        {
+          break;
+        }
+      }
+      Game newGame = new Game(playerOne, playerTwo);
+      newGame.RunGame();
+      Console.WriteLine(newGame.result);
+      break;
+    }
+  }
+}
