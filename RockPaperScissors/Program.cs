332822ee878cca65be9f8588074c0761eb3a4d3b
Jake Edgar <jakeedgar1012@gmail.com>
2022-05-04 16:49:29 -0700

    update to hide user input from fellow user
    Co-authored-by: Caleb Coughenour <CalebCoughenour@protonmail.com>

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -6,16 +6,21 @@ public class Program
 {
   public static void Main()
   {
+    Console.BackgroundColor = ConsoleColor.Black;
     while (true)
     {
       Console.WriteLine("player one choose rock paper or scissors.");
+      Console.ForegroundColor = ConsoleColor.Black;
       string playerOne = Console.ReadLine();
+      Console.ForegroundColor = ConsoleColor.White;
       while (true)
       {
         if ((playerOne != "rock") && (playerOne != "paper") && (playerOne != "scissors"))
         {
           Console.WriteLine("please enter rock paper or scissors.");
+          Console.ForegroundColor = ConsoleColor.Black;
           playerOne = Console.ReadLine();
+          Console.ForegroundColor = ConsoleColor.White;
         }
         else
         {
@@ -23,13 +28,17 @@ public class Program
         }
       }
       Console.WriteLine("player two choose rock paper or scissors.");
+      Console.ForegroundColor = ConsoleColor.Black;
       string playerTwo = Console.ReadLine();
+      Console.ForegroundColor = ConsoleColor.White;
       while (true)
       {
         if ((playerTwo != "rock") && (playerTwo != "paper") && (playerTwo != "scissors"))
         {
           Console.WriteLine("please enter rock paper or scissors.");
+          Console.ForegroundColor = ConsoleColor.Black;
           playerTwo = Console.ReadLine();
+          Console.ForegroundColor = ConsoleColor.White;
         }
         else
         {
